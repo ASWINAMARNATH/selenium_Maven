2531ca22bddc5b5782a9dc2dbf88d8e7a58f58af
ASWINAMARNATH <aswinamarnath718@gmail.com>
2025-05-10 00:43:51 +0530

    c3

--- src/main/java/pages/WorkSpace.java
+++ src/main/java/pages/WorkSpace.java
@@ -1,7 +1,7 @@
 package pages;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
+
 
 import org.openqa.selenium.By;
 
@@ -18,16 +18,16 @@ public class WorkSpace {
     //WebElement workSpaceElement= driver.findElement(By.xpath("//a[text()='Work-Space']"));
 	
      public void clickWorkSpace(){
-    	 Properties prop = null;
-    	 try {
-           prop = new Properties();
-           FileInputStream fis = new FileInputStream("tests/testdata.properties");
-           prop.load(fis);
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-    	 String url=prop.getProperty("url");
-    	 driver.get(url);
+//    	 Properties prop = null;
+//    	 try {
+//           prop = new Properties();
+//           FileInputStream fis = new FileInputStream("tests/testdata.properties");
+//           prop.load(fis);
+//       } catch (IOException e) {
+//           e.printStackTrace();
+//       }
+//    	 String url=prop.getProperty("url");
+    	 driver.get("https://letcode.in/");
     	 WebElement workSpaceElement= driver.findElement(By.xpath("//a[text()='Work-Space']"));
     	 workSpaceElement.click();
      }
